c2fe11e1cadb0536512f1cdd214aee305ff63bc7
dq-mlnck <mlnck@downquark.work>
2021-08-10 02:03:37 -0400

    Minor: removes unused import

--- _src/utils/DesignSystem/_components/templates/tiki.js
+++ _src/utils/DesignSystem/_components/templates/tiki.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit'
+import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import { LayoutTemplateTikiCSS } from '../../_styles/layout/_grid.js'
 
