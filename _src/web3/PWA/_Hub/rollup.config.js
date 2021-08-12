ad5479306fb07a03b3b4d6c735703aa16bc286a0
dq-mlnck <mlnck@downquark.work>
2021-08-12 04:08:46 -0400

    build workds for extension

--- _src/web3/PWA/_Hub/rollup.config.js
+++ _src/web3/PWA/_Hub/rollup.config.js
@@ -16,6 +16,7 @@ export default {
   plugins: [
     resolve(),
     replace({
+      preventAssignment: true,
       "process.env.NODE_ENV": JSON.stringify(
         process.env.NODE_ENV || "production"
       ),
