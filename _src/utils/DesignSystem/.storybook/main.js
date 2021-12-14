30b594c2d3e355e41db71c27d4878585d98d9f1f
JMelnick <jmelnick@atlantistech.com>
2021-12-14 02:26:46 -0500

    code cleanup

--- _src/utils/DesignSystem/.storybook/main.js
+++ _src/utils/DesignSystem/.storybook/main.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   features: {
@@ -27,38 +26,15 @@ module.exports = {
     },
   ],
   "framework": "@storybook/web-components",
-  // webpackFinal: async (config, { configType }) => {
+
   webpackFinal: async (config) => {
-    // const miniCSS = new MiniCssExtractPlugin({
-    //   // Options similar to the same options in webpackOptions.output
-    //   // both options are optional
-    //   filename: "[name].css",
-    //   chunkFilename: "[id].css",
-    // })
-    //   config = {
-    //     ...config,
-    //     plugins: [
-    //       ...config.plugins,
-    //       miniCSS,
-    //     ],
-    //   }
-    config.plugins.push(new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }))
-    
     const scssConfigIndex = config.module.rules.findIndex((config) => ".scss".match(config.test))
     config.module.rules.splice(scssConfigIndex, 1)
-    // console.log('config', config)
 
     config.module.rules.push({
       test: /\.scss$/,
       exclude: /\.module\.scss$/i,
       use: [
-        // fallback to style-loader in development
-        // process.env.NODE_ENV !== "production"
-        //   ? "style-loader"
-        //   : MiniCssExtractPlugin.loader,
         'style-loader',
         'css-loader?url=false',
         'sass-loader'],
@@ -80,7 +56,6 @@ module.exports = {
     test: /\.module\.scss$/i,
     use: [
       {
-        // loader: "style-loader",
           loader: "to-string-loader",
       },
       {
@@ -89,17 +64,8 @@ module.exports = {
           importLoaders: 1,
           modules: {
             compileType: 'module',
-            // mode: "local",
-            //// the above gives us something at least
-            // compileType: "module",
-            // mode: "local",
-            // auto: true,
             exportGlobals: true,
-            // localIdentName: "[path][name]__[local]--[hash:base64:5]",
             localIdentName: "[name]",
-            // localIdentContext: path.resolve(__dirname, "src"),
-            // localIdentHashPrefix: "my-custom-hash",
-            // namedExport: true,
             exportLocalsConvention: "camelCaseOnly",
             exportOnlyLocals: false,
           },
@@ -115,19 +81,3 @@ module.exports = {
     return config;
   },
 }
-
-/*
-{
-      name: '@storybook/preset-scss',
-      options: {
-        cssLoaderOptions: {
-          sourceMap: true,
-          modules: { localIdentName },
-        },
-        sassLoaderOptions: {
-          implementation: require("sass"), // prefer Dart-Sass
-          sourceMap: true
-        },
-      },
-    },
-*/
